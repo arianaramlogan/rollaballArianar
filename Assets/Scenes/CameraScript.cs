7da94fe48b43367a45dbc7b4f09d84569a43eec9
arianaramlogan <arianaramlogan34@gmail.com>
2025-11-04 09:50:25 -0500

    11/4/25

---
+++ Assets/Scenes/CameraScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        offset = transform.position-player.transform.position;
+    }
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
--- Assets/Scenes/Playercontroller.cs
+++ Assets/Scenes/Playercontroller.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Playercontroller : MonoBehaviour
 {
+
+
+    private Rigidbody rb;
+
+    private float MovementX;
+    private float MovementY;
+
+    public float speed = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+   private void FixedUpdate()
     {
-        
+        Vector3 Movement = new Vector3 (MovementX, 0.0f, MovementY);
+
+        rb.AddForce(Movement*speed);
+    }
+
+    void OnMove(InputValue MovementValue)
+    {
+        Vector2 MovementVector= MovementValue.Get<Vector2>();
+        MovementX = MovementVector.x;
+        MovementY = MovementVector.y;
     }
 }
