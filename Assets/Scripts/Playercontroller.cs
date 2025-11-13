74ba4dfd49e6f69f694ddbbce14e2f471a5b5d32
arianaramlogan <arianaramlogan34@gmail.com>
2025-11-13 09:59:45 -0500

    11/13/25

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -31,4 +31,14 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+ if (other.gameObject.CompareTag("PickUp")) 
+        {
+ // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+    }
+ }
 }
